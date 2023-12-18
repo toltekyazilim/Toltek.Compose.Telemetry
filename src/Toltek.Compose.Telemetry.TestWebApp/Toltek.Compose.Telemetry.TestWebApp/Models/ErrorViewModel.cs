31147e4dfb97ac49234469461ace4f44526c770f
Yavuz coşar <yavuzcosar@outlook.com>
2023-12-18 17:16:04 +0300

    add test app

---
+++ src/Toltek.Compose.Telemetry.TestWebApp/Toltek.Compose.Telemetry.TestWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Toltek.Compose.Telemetry.TestWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
