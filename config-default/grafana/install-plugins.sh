e9cbdea91b0ebf16d2886fa58f90ded254d3867f
Yavuz coşar <yavuzcosar@outlook.com>
2023-12-20 01:14:58 +0300

    update

--- config-default/grafana/install-plugins.sh
+++
@@ -1,6 +0,0 @@
-docker exec -it toltek_grafana /bin/bash
-	grafana cli plugins install redis-datasource
-  grafana cli plugins install grafana-mongodb-datasource
-	grafana cli plugins install grafana-piechart-panel
-	exit
-docker restart toltek_grafana
